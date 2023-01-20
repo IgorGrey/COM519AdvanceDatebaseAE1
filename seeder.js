aedbec67ebffd78e1a7ec3b06630b62e392b24e1
Igor Kanchukovskyi <mail.igorkan@gmail.com>
2023-01-20 14:49:11 +0000

    Update seeder.js

--- seeder.js
+++ seeder.js
@@ -6,11 +6,7 @@ const loading = require("loading-cli");
 const Rlp = require("./models/Personnel_loss");
 const Rle = require("./models/Equipment_loss");
 
-const { MONGODB_URI, MONGODB__PRODUCTION_URI } = process.env;
-
-const client = new MongoClient(
-  process.env.NODE_ENV === "production" ? MONGODB__PRODUCTION_URI : MONGODB_URI
-);
+const { MONGODB_URI } = process.env;
 
 async function main() {
     try {
